b039f61b77f559539c4d1138c6840372ed3babb9
Brooke Barbee <109841386+bbarbee@users.noreply.github.com>
2023-11-10 21:20:16 -0600

    Working javascript
    added javascript to add to the to do list and remove from the to do list

--- script.js
+++ script.js
@@ -7,32 +7,42 @@ UPDATE/COMPLETE TASKS
 READ TASKS
 DELETE TASKS */
 
-// create variables for elements
-
+const addButton = document.getElementById('addButton');
 const userInput = document.getElementById('userInput');
 const toDoList = document.getElementById('toDoList');
 
+// Add an event listener to the add button
+addButton.addEventListener('click', addTask);
 
-//create a function to add user input to the toDoList when the click the add button
+function addTask() {
+    // Get the user input
+    let taskText = userInput.value.trim();
 
-function userUpdatedToDoList(task) {
-    let userUpdatedToDoList = document.getElementById('userUpdatedToDoList');
-    let li = document.createElement('li');
-    li.textContent = task;
-    userUpdatedToDoList.appendChild(li);
-}
-
-function addTask(){
-    let task = userInput.value.trim();//chat gpt suggestion
-    if(userInput.value === ''){
-        alert("It must be mice to have nothing to do!");
-    }
-    else{
+    // Check if the user input is not empty
+    if (taskText) {
+        // Create a new list item
         let li = document.createElement('li');
-        li.textContent = task;
-        toDoList.appendChild(li);
-        userInput.value = '';
-        userUpdatedToDoList(task);
-    }
+        li.textContent = taskText;
 
+        //Add a button to remove li from the to do list
+        let removeButton= document.createElement('button');
+        removeButton.textContent = 'X';
+        removeButton.classList.add('removeButtons');
+
+        removeButton.addEventListener('click', function() {
+            li.remove();
+        }
+        )
+
+        li.appendChild(removeButton);
+
+        // Add the list item to the to-do list
+        toDoList.appendChild(li);
+
+        userInput.value = '';
+    } else {
+        alert("Please enter a task!");
+    }
 }
+
+//create a function to complete the task
